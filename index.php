ba86d2b21b3d155692ef7a3d19db679251faa423
HippoTlm <hippolyte.toulemonde@gmail.com>
2017-10-16 16:04:04 +0200

    Correction et police
    Ellipsis au lieu d'Ellispsis et nouvelle police plus fine

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 </head>
 <body class="defaultBody">
 	<?php
-		$titre = 'Ellispsis Architecture';
-		$subTitle = "Cabinet d'architectes à villeneuve d'Ascq";
+		$titre = 'Ellipsis Architecture';
+		$subTitle = "Cabinet d'architectes à Villeneuve d'Ascq";
 		include('include/titre.php');
 		$path =  'assets/img/index';
 		$menuPath = 'assets/img/menu/index';
 		$menuUrl = 'categories.php?cat=';
-		include('include/squaredImages.php'); 
+		include('include/squaredImages.php');
 	?>
 	<script language="javascript" type="text/javascript" src="http://localhost/EllipsisArchi/assets/js/main.js"></script>
 </body>
