60c416ee032716655e21528815ed7794b5a91a02
Anko59 <mariedelahouce@gmx.fr>
2017-10-18 14:58:25 +0200

    Merge really resolved

--- index.php
+++ index.php
@@ -5,18 +5,14 @@
 </head>
 <body class="defaultBody">
 	<?php
-		$titre = 'Ellipsis Architecture';
-		$subTitle = "Cabinet d'architectes à Villeneuve d'Ascq";
+		$titre = 'Ellispsis Architecture';
+		$subTitle = "Cabinet d'architectes à villeneuve d'Ascq";
 		include('include/titre.php');
 		$path =  'assets/img/index';
 		$menuPath = 'assets/img/menu/index';
 		$menuUrl = 'categories.php?cat=';
-<<<<<<< HEAD
 		$condition = 'is_main_index=1 ORDER BY RAND()';
 		include('include/squaredImages.php'); 
-=======
-		include('include/squaredImages.php');
->>>>>>> 166054f7a9cb49ca583f8fd8d7fb8784484ebdac
 	?>
 	<script language="javascript" type="text/javascript" src="http://localhost/EllipsisArchi/assets/js/main.js"></script>
 </body>
